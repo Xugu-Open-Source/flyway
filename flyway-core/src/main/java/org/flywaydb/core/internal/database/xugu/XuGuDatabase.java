81a3dcb5fca44316d1c9f031b6ce408c54fe4d3f
吴启洋 <wqy@xugudb.com>
2024-06-28 11:16:45 +0800

    适配xugu

---
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/xugu/XuGuDatabase.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright © Red Gate Software Ltd 2010-2020
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.core.internal.database.xugu;
+
+import org.flywaydb.core.api.FlywayException;
+import org.flywaydb.core.api.MigrationType;
+import org.flywaydb.core.api.MigrationVersion;
+import org.flywaydb.core.api.configuration.Configuration;
+import org.flywaydb.core.api.logging.Log;
+import org.flywaydb.core.api.logging.LogFactory;
+import org.flywaydb.core.internal.database.base.Database;
+import org.flywaydb.core.internal.database.base.DatabaseType;
+import org.flywaydb.core.internal.database.base.Table;
+import org.flywaydb.core.internal.database.mysql.mariadb.MariaDBDatabaseType;
+import org.flywaydb.core.internal.exception.FlywaySqlException;
+import org.flywaydb.core.internal.jdbc.JdbcConnectionFactory;
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.flywaydb.core.internal.jdbc.StatementInterceptor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * MySQL database.
+ */
+public class XuGuDatabase extends Database<XuGuConnection> {
+    // See https://mariadb.com/kb/en/version/
+
+    /**
+     * Creates a new instance.
+     *
+     * @param configuration The Flyway configuration.
+     */
+    public XuGuDatabase(Configuration configuration, JdbcConnectionFactory jdbcConnectionFactory, StatementInterceptor statementInterceptor) {
+        super(configuration, jdbcConnectionFactory, statementInterceptor);
+    }
+
+    @Override
+    public String getRawCreateScript(Table table, boolean baseline) {
+        String tablespace = configuration.getTablespace() == null
+                ? ""
+                : " TABLESPACE \"" + configuration.getTablespace() + "\"";
+
+        return "CREATE TABLE " + table + " (\n" +
+                "    \"installed_rank\" INT NOT NULL,\n" +
+                "    \"version\" VARCHAR2(50),\n" +
+                "    \"description\" VARCHAR2(200) NOT NULL,\n" +
+                "    \"type\" VARCHAR2(20) NOT NULL,\n" +
+                "    \"script\" VARCHAR2(1000) NOT NULL,\n" +
+                "    \"checksum\" INT,\n" +
+                "    \"installed_by\" VARCHAR2(100) NOT NULL,\n" +
+                "    \"installed_on\" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,\n" +
+                "    \"execution_time\" INT NOT NULL,\n" +
+                "    \"success\" NUMBER(1) NOT NULL,\n" +
+                "    CONSTRAINT \"" + table.getName() + "_pk\" PRIMARY KEY (\"installed_rank\")\n" +
+                ")" + tablespace + ";\n" +
+                (baseline ? getBaselineStatement(table) + ";\n" : "") +
+                "CREATE INDEX \"" + table.getSchema().getName() + "\".\"" + table.getName() + "_s_idx\" ON " + table + " (\"success\");\n";
+    }
+
+    @Override
+    protected XuGuConnection doGetConnection(Connection connection) {
+        return new XuGuConnection(this, connection);
+    }
+
+    @Override
+    public final void ensureSupported() {
+        ensureDatabaseIsRecentEnough("11");
+
+        ensureDatabaseNotOlderThanOtherwiseRecommendUpgradeToFlywayEdition("11.0", org.flywaydb.core.internal.license.Edition.ENTERPRISE);
+
+        recommendFlywayUpgradeIfNecessary("12.0");
+    }
+
+    @Override
+    protected String doGetCurrentUser() throws SQLException {
+        return getMainConnection().getJdbcTemplate().queryForString("SELECT USER FROM DUAL");
+    }
+
+    @Override
+    public boolean supportsDdlTransactions() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsChangingCurrentSchema() {
+        return true;
+    }
+
+    @Override
+    public String getBooleanTrue() {
+        return "1";
+    }
+
+    @Override
+    public String getBooleanFalse() {
+        return "0";
+    }
+
+    @Override
+    public String doQuote(String identifier) {
+        return "`" + identifier + "`";
+    }
+
+    @Override
+    public boolean catalogIsSchema() {
+        return true;
+    }
+}
