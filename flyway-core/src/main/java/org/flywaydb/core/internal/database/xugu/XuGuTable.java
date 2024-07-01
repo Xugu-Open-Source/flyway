9010d719499eac730610d23f5b6ef666c679d8ce
吴启洋 <wqy@xugudb.com>
2024-07-01 17:06:35 +0800

    初步完成适配虚谷

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/xugu/XuGuTable.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/xugu/XuGuTable.java
@@ -1,37 +1,10 @@
-/*
- * Copyright © Red Gate Software Ltd 2010-2020
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.flywaydb.core.internal.database.xugu;
 
 import org.flywaydb.core.internal.database.base.Table;
 import org.flywaydb.core.internal.jdbc.JdbcTemplate;
-
 import java.sql.SQLException;
 
-/**
- * MySQL-specific table.
- */
 public class XuGuTable extends Table<XuGuDatabase, XuGuSchema> {
-    /**
-     * Creates a new MySQL table.
-     *
-     * @param jdbcTemplate The Jdbc Template for communicating with the DB.
-     * @param database     The database-specific support.
-     * @param schema       The schema this table lives in.
-     * @param name         The name of the table.
-     */
     XuGuTable(JdbcTemplate jdbcTemplate, XuGuDatabase database, XuGuSchema schema, String name) {
         super(jdbcTemplate, database, schema, name);
     }
