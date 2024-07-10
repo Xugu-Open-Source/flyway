dc9472585ef2c61afe88f36a0f0e91caf29a0c85
吴启洋 <wqy@xugudb.com>
2024-07-10 17:41:31 +0800

    调整flyway支持xugu使用maven插件执行表结构创建

--- flyway-core/src/main/java/org/flywaydb/core/internal/database/xugu/XuGuDatabaseType.java
+++ flyway-core/src/main/java/org/flywaydb/core/internal/database/xugu/XuGuDatabaseType.java
@@ -1,17 +1,29 @@
+/*
+ * Copyright © Red Gate Software Ltd 2010-2020
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.flywaydb.core.internal.database.xugu;
 
 import org.flywaydb.core.api.ResourceProvider;
 import org.flywaydb.core.api.configuration.Configuration;
-import org.flywaydb.core.internal.callback.CallbackExecutor;
 import org.flywaydb.core.internal.database.base.Database;
 import org.flywaydb.core.internal.database.base.DatabaseType;
 import org.flywaydb.core.internal.jdbc.JdbcConnectionFactory;
-import org.flywaydb.core.internal.jdbc.JdbcTemplate;
 import org.flywaydb.core.internal.jdbc.StatementInterceptor;
 import org.flywaydb.core.internal.parser.Parser;
 import org.flywaydb.core.internal.parser.ParsingContext;
-import org.flywaydb.core.internal.sqlscript.SqlScriptExecutor;
-import org.flywaydb.core.internal.sqlscript.SqlScriptExecutorFactory;
+import org.flywaydb.core.internal.util.ClassUtils;
 
 import java.sql.Connection;
 import java.sql.Types;
@@ -38,14 +50,17 @@ public class XuGuDatabaseType extends DatabaseType {
         return url.startsWith("jdbc:xugu:");
     }
 
-//    @Override
-//    public Pattern getJDBCCredentialsPattern() {
-//        return usernamePasswordPattern;
-//    }
-
     @Override
     public String getDriverClass(String url, ClassLoader classLoader) {
         return "com.xugu.cloudjdbc.Driver";
+    }
+
+    @Override
+    public String getBackupDriverClass(String url, ClassLoader classLoader) {
+        if (ClassUtils.isPresent(XUGU_LEGACY_JDBC_DRIVER, classLoader)) {
+            return XUGU_LEGACY_JDBC_DRIVER;
+        }
+        return null;
     }
 
     @Override
@@ -64,25 +79,16 @@ public class XuGuDatabaseType extends DatabaseType {
     }
 
     @Override
-    public SqlScriptExecutorFactory createSqlScriptExecutorFactory(JdbcConnectionFactory jdbcConnectionFactory,
-                                                                   final CallbackExecutor callbackExecutor,
-                                                                   final StatementInterceptor statementInterceptor
-    ) {
-        final DatabaseType thisRef = this;
-        return new SqlScriptExecutorFactory() {
-            @Override
-            public SqlScriptExecutor createSqlScriptExecutor(Connection connection , boolean undo, boolean batch, boolean outputQueryResults) {
-                return new XuGuSqlScriptExecutor(new JdbcTemplate(connection, thisRef), callbackExecutor, undo, batch, outputQueryResults, statementInterceptor);
-            }
-        };
-    }
-
-    @Override
     public void setDefaultConnectionProps(String url, Properties props, ClassLoader classLoader) {
-
+        props.put("connectionAttributes", "program_name:" + APPLICATION_NAME);
     }
 
     @Override
     public void setConfigConnectionProps(Configuration config, Properties props, ClassLoader classLoader) {
+    }
+
+    @Override
+    public boolean detectPasswordRequiredByUrl(String url) {
+        return super.detectPasswordRequiredByUrl(url);
     }
 }
